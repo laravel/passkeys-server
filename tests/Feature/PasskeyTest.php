d4f5518e8e86ee3f6e0bbc284513f604f5ce1aef
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2026-05-14 11:49:04 -0700

    Fix passkey user relation with custom user keys (#23)

--- tests/Feature/PasskeyTest.php
+++ tests/Feature/PasskeyTest.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Schema;
+use Laravel\Passkeys\Contracts\PasskeyUser;
 use Laravel\Passkeys\Passkey;
+use Laravel\Passkeys\PasskeyAuthenticatable;
+use Laravel\Passkeys\Passkeys;
 
 it('can instantiate the passkey model', function (): void {
     $passkey = new Passkey;
@@ -17,3 +23,37 @@ it('has the correct fillable attributes', function (): void {
         'credential',
     ]);
 });
+
+it('can belong to a user model with a custom primary key', function (): void {
+    Schema::create('users_with_custom_primary_keys', function (Blueprint $table): void {
+        $table->unsignedBigInteger('user_id')->primary();
+    });
+
+    Passkeys::useUserModel(UserWithCustomPrimaryKey::class);
+
+    $user = UserWithCustomPrimaryKey::create([
+        'user_id' => 1234,
+    ]);
+
+    $passkey = (new Passkey)->forceFill([
+        'user_id' => $user->getKey(),
+    ]);
+
+    expect($passkey->user)->not->toBeNull();
+    expect($passkey->user->is($user))->toBeTrue();
+});
+
+class UserWithCustomPrimaryKey extends Authenticatable implements PasskeyUser
+{
+    use PasskeyAuthenticatable;
+
+    protected $table = 'users_with_custom_primary_keys';
+
+    protected $primaryKey = 'user_id';
+
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+    protected $guarded = [];
+}
